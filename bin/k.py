54ef494a5b7a7c7e07ecfe6cf7d13f4b992c65b9
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-11-29 11:25:45 +0100

    Created bin/k.py

---
+++ bin/k.py
@@ -0,0 +1,19 @@
+"""k
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-11-29"
+
+class k:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(k.k())
+	
+	def k():
+		"""k first funtion
+		"""
+		return "k" #for the test (see test_k.py)
+		
+if __name__ == "__main__":
+	k()
